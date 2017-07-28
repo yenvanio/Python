a19bb6d44668299b25f532a384f13cf2481c424f
yenvanio <yoganathan.shiv@gmail.com>
2017-07-27 23:11:52 -0400

    Info Update

---
+++ sched.py
@@ -0,0 +1,10 @@
+#Table Header Class ="ddheader"/> is used for each of the class names
+#Find Next Table Class = "bordertable" and Summary = "This table lists the scheduled meeting times and assigned instructors for this class"
+#Or Next Caption (class = captiontext) = "Scheduled Meeting Times"
+#find next TD class="dbdefault" while next is dbdefault, if dddefault then stop
+    #Week (If empty then every week)
+    #Time
+    #Day
+    #Location (If ONLINE or N/A then dont include)
+    #Date Range (Gives Start and End, For Labs gives which dates Labs Are)
+    #Type of Class
