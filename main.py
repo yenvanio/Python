05055c7b775c88732336ac698d338c9b59600ea2
yenvanio <yoganathan.shiv@gmail.com>
2017-07-28 00:02:13 -0400

    Testing on Main

--- main.py
+++ main.py
@@ -1,41 +1,6 @@
+import urllib3
+http = urllib3.PoolManager()
+response = http.request('GET', 'http://www.crummy.com/software/BeautifulSoup/')
+
 from bs4 import BeautifulSoup
-import requests
-import re
-
-minPrice = '100000'
-maxPrice = '800000'
-
-bedRange = '3-0'
-bathRange = '3-3'
-
-curPage = '1'
-
-address_id = 'hm_lst_address'
-address_class = 'm_gallery_lst_cell_address clickable'
-
-price_id = 'hm_lst_price'
-price_class = 'm_gallery_lst_cell_price'
-
-bed_id = 'hm_lst_bed_num'
-bath_id = 'hm_lst_bath_num'
-bed_bath_class = 'm_gallery_lst_cell_bed_bath' #get spans inside
-
-url  = "https://www.realtor.ca/Residential/Map.aspx#CultureId=1&ApplicationId=1"
-url += "&RecordsPerPage=9&MaximumResults=9&PropertySearchTypeId=1&PriceMin=" + minPrice
-url += "&PriceMax=" + maxPrice +"&TransactionTypeId=2&StoreyRange=0-0&BuildingTypeId=1"
-url += "&ConstructionStyleId=3&BedRange=" + bedRange + "&BathRange=" + bathRange + ""
-url += "&LongitudeMin=-79.24541473388672&LongitudeMax=-78.9364242553711&LatitudeMin=43.79563252135698"
-url += "&LatitudeMax=43.92930267631971&SortOrder=A&SortBy=1&viewState=g&favouritelistingids=18230582,18292971,18297982"
-url += "&Longitude=-79.0747833251953&Latitude=43.8983871698998&ZoomLevel=11&CurrentPage=" + curPage
-url += "&PropertyTypeGroupID=1"
-
-r  = requests.get(url)
-
-sauce = r.text
-
-soup = BeautifulSoup(sauce, "html.parser")
-
-print soup
-
-for div in soup.find_all('div', attrs={"class": address_class}):
-    print div.text
+soup = BeautifulSoup(response.data)
--- sched.py
+++ sched.py
@@ -1,3 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+term = '201709'
+d = 'dummy'
+subj = ''
+crse = '_____'
+
+class prep(object):
+    def __init__(self):
+        self.search_request = {
+            'TRM': 'U'
+            ,'term_in':term
+            ,'sel_subj':d,'sel_day':d,'sel_schd':d,'sel_insm':d,'sel_camp':d,'sel_levl':d,'sel_sess':d,'sel_instr':d,'sel_ptrm':d,'sel_attr':d
+            ,'sel_subj':subj
+            ,'sel_crse':crse
+            ,'sel_title':""
+            ,'sel_schd':""
+            ,'sel_insm':"%25"
+            ,'sel_from_cred':""
+            ,'sel_to_cred':""
+            ,'sel_camp':"%25"
+            ,'begin_hh':0
+            ,'begin_mi':0
+            ,'end_hh':0
+            ,'end_mi':0
+            ,'end_ap':0
+        }
+
+        self.headers = {'Referer': 'https://ssbp.mycampus.ca/prod_uoit/bwckschd.p_get_crse_unsec'}
+
+def scrape(self):
+            payload = json.dumps(self.search_request)
+            r = requests.post('https://ssbp.mycampus.ca/prod_uoit/bwckschd.p_get_crse_unsec', data=payload ,headers=self.headers)
+            s = BeautifulSoup(r.text)
+            print(s)
+
 #Table Header Class ="ddheader"/> is used for each of the class names
 #Find Next Table Class = "bordertable" and Summary = "This table lists the scheduled meeting times and assigned instructors for this class"
 #Or Next Caption (class = captiontext) = "Scheduled Meeting Times"
@@ -8,3 +46,7 @@
     #Location (If ONLINE or N/A then dont include)
     #Date Range (Gives Start and End, For Labs gives which dates Labs Are)
     #Type of Class
+
+if __name__ == '__main__':
+    scraper = prep()
+    scraper.scrape()
